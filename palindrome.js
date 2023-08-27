6cf5af4faf449eb7f4feec4ee4baecc8a69e4500
vijendra2244 <vs230267@gmail.com>
2023-08-27 20:18:48 +0530

    added a palindrome

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+function Palindrome(num){
+       let str="";
+       str+=num
+       let sum="";
+       
+       for(let i=str.length-1;i>=0;i--){
+          sum+= str[n]
+        
+       }console.log(sum)
+        let bag=sum;
+       if(bag==num){
+           console.log("Yes")
+       }
+       else{
+           console.log("No")
+       }
+    }
+    
