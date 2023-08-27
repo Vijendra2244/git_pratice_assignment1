86a51e2593cd0fc8cba84a3f5c4cffdc4cde33a7
vijendra2244 <vs230267@gmail.com>
2023-08-27 19:50:58 +0530

    added a prime number code

---
+++ prime.js
@@ -0,0 +1,15 @@
+function checkPrime(num) {
+  let count = 0;
+  for (let i = 0; i <= num; i++) {
+    if (num % num == 0) {
+      count++;
+    }
+  }
+  if (count >= 2) {
+    return "Prime";
+  } else {
+    return "Not Prime";
+  }
+}
+let prime = checkPrime(23);
+console.log(prime);
